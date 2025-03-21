f0b5dfb1b1b861f8953e33a7b30a66c5c67c0a96
Jacob Favela <jfavela@chapman.edu>
2025-03-21 12:12:25 -0700

    Add files via upload

---
+++ AdventureS25/CommandHandler.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventureS25;
+
+public static class CommandHandler
+{
+    private static Dictionary<string, Action<Command>> commandMap =
+        new Dictionary<string, Action<Command>>()
+        {
+            {"eat", Eat},
+            {"go", Move},
+            {"tron", Tron},
+            {"troff", Troff},
+            {"take", Take},
+            {"inventory", ShowInventory},
+            {"look", Look},
+            {"drop", Drop},
+            {"nouns", Nouns},
+            {"verbs", Verbs}
+        };
+
+    private static void Verbs(Command command)
+    {
+        List<string> verbs = CommandValidator.GetVerbs();
+        foreach (string verb in verbs)
+        {
+            Console.WriteLine(verb);
+        }
+    }
+
+    private static void Nouns(Command command)
+    {
+        List<string> nouns = CommandValidator.GetNouns();
+        foreach (string noun in nouns)
+        {
+            Console.WriteLine(noun);
+        }
+    }
+
+    public static void Handle(Command command)
+    {
+        if (commandMap.ContainsKey(command.Verb))
+        {
+            Action<Command> method = commandMap[command.Verb];
+            method.Invoke(command);
+        }
+        else
+        {
+            Console.WriteLine("I don't know how to do that.");
+        }
+    }
+    
+    private static void Drop(Command command)
+    {
+        Player.Drop(command);
+    }
+    
+    private static void Look(Command command)
+    {
+        Player.Look();
+    }
+
+    private static void ShowInventory(Command command)
+    {
+        Player.ShowInventory();
+    }
+    
+    private static void Take(Command command)
+    {
+        Player.Take(command);
+    }
+
+    private static void Troff(Command command)
+    {
+        Debugger.Troff();
+    }
+
+    private static void Tron(Command command)
+    {
+        Debugger.Tron();
+    }
+
+    public static void Eat(Command command)
+    {
+        Console.WriteLine("Eating..." + command.Noun);
+    }
+
+    public static void Move(Command command)
+    {
+        Player.Move(command);
+    }
+}
---
+++ AdventureS25/Game.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventureS25;
+
+public static class Game
+{
+    public static void PlayGame()
+    {
+        Initialize();
+
+        Console.WriteLine(Player.GetLocationDescription());
+        
+        bool isPlaying = true;
+        
+        while (isPlaying == true)
+        {
+            Command command = CommandProcessor.Process();
+            
+            if (command.IsValid)
+            {
+                if (command.Verb == "exit")
+                {
+                    Console.WriteLine("Game Over!");
+                    isPlaying = false;
+                }
+                else
+                {
+                    CommandHandler.Handle(command);
+                }
+            }
+        }
+    }
+
+    private static void Initialize()
+    {
+        Map.Initialize();
+        Items.Initialize();
+        Player.Initialize();
+    }
+}
